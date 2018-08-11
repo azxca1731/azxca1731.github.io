855910bc14e5eb8f2d56aa73aac2f62071df8d6e
nogada <azxca1731@gmail.com>
2018-08-11 18:00:56 +0900

    add discus and TODO

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import PageHeader from '../component/PageHeader';
 import PostParagraph from '../component/PostParagraph';
+import { DiscussionEmbed } from 'disqus-react';
 //포스트 예시 나중에 바꿀것
+const disqusConfig = {
+    url: "https://azxca1731.github.io/#/posts",
+    identifier: "1",
+    title: "포스트 예시",
+};
 const Posts = () => {
     return (
         <div>
@@ -10,7 +16,8 @@ const Posts = () => {
                 <div className="row">
                     <div class="col-lg-8 col-md-10 mx-auto">
                         <PostParagraph title="이렇게 된다" upp="진짜 기나긴 하루 끝에 자고 싶다." underp="바로 연속으로 할려면 이렇겡!" />
-                        <PostParagraph upp="이러면 연속으로 쓰이고 그리고 패러그래프도 나뉜다." />
+                        <DiscussionEmbed upp="이러면 연속으로 쓰이고 그리고 패러그래프도 나뉜다." />
+                        <DiscussionEmbed shortname="nogada-samuso" config={disqusConfig} />
                     </div>
                 </div>
             </div>
